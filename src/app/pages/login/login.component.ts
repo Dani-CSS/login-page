abdee3d130c367203653d4a9e28f58ce6e0c9f32
Daniele <danielecs26@gmail.com>
2024-07-21 00:16:08 -0300

    feat: add default login layout

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 import { DefaultLoginLayoutComponent } from '../../components/default-login-layout/default-login-layout.component';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { PrimaryInputComponent } from '../../components/primary-input/primary-input.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
     DefaultLoginLayoutComponent,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    PrimaryInputComponent
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
@@ -17,8 +19,8 @@ export class LoginComponent {
 
   constructor(){
     this.loginForm = new FormGroup({
-      email: new FormControl(''. [Validators.required, Validators.email]),
-      password: new FormControl(''. [Validators.required, Validators.minLength(6)])
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)])
     })
   }
 
