4f1f799b73f40d086b4e63d891785a74dc6aa0d1
Dani-CSS <danielecs26@gmail.com>
2025-10-08 12:47:19 -0300

    feat: add register/user pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { SignUpComponent } from './pages/signup/signup.component';
+import { AuthGuard } from './services/auth-guard.service';
+import { UserComponent } from './pages/user/user.component';
 
 
 
@@ -14,6 +16,12 @@ export const routes: Routes = [
   {
     path: "signup",
     component: SignUpComponent
+  },
+
+  {
+    path: "user",
+    component: UserComponent,
+    canActivate: [AuthGuard]
   }
 
 ];
